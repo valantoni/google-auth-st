f50ffced3e36bd8167b8fe9a4e4345eb73abc763
Antonio V <avalverde@stroeer.de>
2024-05-13 14:15:42 +0200

    update setup

--- setup.py
+++ setup.py
@@ -5,11 +5,7 @@ setup(
     version='0.2',
     packages=find_packages(),
     description='Librería para añadir autenticación con Google en aplicaciones web con el framework de Streamlit.',
-<<<<<<< HEAD
     long_description=open('README.md',encoding='utf-8').read(),
-=======
-    long_description=open('README.md', encoding = 'utf-8').read(),
->>>>>>> cb8faabe321a4660f3a03efe185566e4aedf6568
     long_description_content_type='text/markdown',  
     author='Toni Dev',
     author_email='classtonidev@gmail.com',
