69b7948307bfa2417608dbaf93ad7259c31e2af7
Antonio V <avalverde@stroeer.de>
2024-05-13 16:12:33 +0200

    error solved

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='google-auth-st',
-    version='1.0',
+    version='1.1',
     packages= ['google_auth_st'],
     description='Librería para añadir autenticación con Google en aplicaciones web con el framework de Streamlit.',
     long_description=open('README.md',encoding='utf-8').read(),
