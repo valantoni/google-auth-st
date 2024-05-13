01df2d85e5c7dbb63885d3cae645315dda3fa129
Antonio V <avalverde@stroeer.de>
2024-05-13 15:55:38 +0200

    Resolved conflicts

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='google-auth-st',
-    version='0.2',
-    packages=find_packages(),
+    version='0.3',
+    packages= ['google_auth_st'],
     description='Librería para añadir autenticación con Google en aplicaciones web con el framework de Streamlit.',
     long_description=open('README.md',encoding='utf-8').read(),
     long_description_content_type='text/markdown',  
